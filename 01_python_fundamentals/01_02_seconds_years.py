e55242a63ef733d1b724f606b53c2c304ce47640
ljohnson521 <76568336+ljohnson521@users.noreply.github.com>
2020-12-25 10:27:59 -0700

    updated to add seconds in year solution

--- 01_python_fundamentals/01_02_seconds_years.py
+++ 01_python_fundamentals/01_02_seconds_years.py
@@ -4,3 +4,10 @@ Move the code you previously wrote to calculate how many seconds are in a year i
 Then execute it as a script to see the output printed to your console.
 
 '''
+print(60 * 60)
+secs_in_hour = 3600
+print(3600 * 24)
+secs_in_day = 86400
+days_in_year = 365
+secs_in_year = (days_in_year * secs_in_day)
+print(secs_in_year)
