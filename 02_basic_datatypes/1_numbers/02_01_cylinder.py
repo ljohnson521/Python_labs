c8f5faab24b969e29f4685745427b9675d758039
ljohnson521 <76568336+ljohnson521@users.noreply.github.com>
2020-12-28 11:52:50 -0700

    Update 02_01_cylinder.py
    complete

--- 02_basic_datatypes/1_numbers/02_01_cylinder.py
+++ 02_basic_datatypes/1_numbers/02_01_cylinder.py
@@ -4,3 +4,7 @@ of a cylinder with a radius of 3.14 and a height of 5. Print out the result.
 
 
 '''
+Import math
+s_area = 2 * math.pi * 3.14 * (3.14 + 5)
+vol = math.pi * 3.14 * 5
+print(s_area, vol)
