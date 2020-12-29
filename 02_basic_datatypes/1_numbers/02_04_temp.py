b3eeb93457d284cb24c069aab7848928277d6419
ljohnson521 <76568336+ljohnson521@users.noreply.github.com>
2020-12-28 23:40:50 -0700

    Update 02_04_temp.py
    complete

--- 02_basic_datatypes/1_numbers/02_04_temp.py
+++ 02_basic_datatypes/1_numbers/02_04_temp.py
@@ -10,3 +10,7 @@ Output should read like - "81.32 degrees fahrenheit = 27.4 degrees celsius"
 
 
 '''
+
+Fahrenheit = float(input("Enter a temperature in Fahrenheit: "))
+Celsius = (Fahrenheit - 32) * 5.0/9.0
+print('%.2f degrees Fahrenheit = %0.2f degrees Celsius' %(Fahrenheit, Celsius))
