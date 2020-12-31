7d8c0a12ec3b36a6e4066dea60fa49b8468edd5b
ljohnson521 <76568336+ljohnson521@users.noreply.github.com>
2020-12-31 14:16:56 -0700

    Update 02_06_invest.py
    complete

--- 02_basic_datatypes/1_numbers/02_06_invest.py
+++ 02_basic_datatypes/1_numbers/02_06_invest.py
@@ -7,3 +7,10 @@ Take in the following three values from the user:
 Print the future values to the console.
 
 '''
+
+p_values = float(input("How much would you like to invest: "))
+inv_values = float(input("what's your interest rate: "))
+duration = float(input('For how long: '))
+interest = float(inv_values / 100)
+future_value = float(p_values * (1 + interest) ** duration)
+print(future_value)
