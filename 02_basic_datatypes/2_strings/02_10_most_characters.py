6053bc426482332ec2aa86ab85776639e69a9cff
ljohnson521 <76568336+ljohnson521@users.noreply.github.com>
2021-01-03 21:46:57 -0700

    Update 02_10_most_characters.py
    complete

--- 02_basic_datatypes/2_strings/02_10_most_characters.py
+++ 02_basic_datatypes/2_strings/02_10_most_characters.py
@@ -11,3 +11,15 @@ CHALLENGE: Can you edit to script to print only the string with the most charact
            into the topic "Conditionals" to solve this challenge.
 
 '''
+test1 = input("Enter sentence here: ")
+test_one = len(test1)
+test2 = input("Enter sentence here: ")
+test_two = len(test2)
+test3 = input("Enter sentence here: ")
+test_three = len(test3)
+final_test = {test_three: test3, test_two: test2, test_one: test1}
+for test in final_test.items():
+    print(test)
+test_values = final_test.values()
+max_values = max(test_values)
+print(max_values)
