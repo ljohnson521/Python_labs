c0487ff23dd02916e2acec3b72c121292fc9b586
ljohnson521 <76568336+ljohnson521@users.noreply.github.com>
2021-01-03 17:24:16 -0700

    Update 02_09_vowel.py
    complete

--- 02_basic_datatypes/2_strings/02_09_vowel.py
+++ 02_basic_datatypes/2_strings/02_09_vowel.py
@@ -6,3 +6,26 @@ CHALLENGE: Can you change the script so that it counts the occurrence of each in
            in the string and print a count for each of them?
 
 '''
+input_string = input("enter sentence here: ")
+string = input_string.lower()
+print(string)
+count = 0
+list1 = ['a', 'e', 'i', 'o', 'u']
+for char in string:
+    if char in list1:
+        count = count+1
+print(count)
+
+new_string = input('enter sentence here: ')
+new_string1 = new_string.lower()
+
+vowels = "aeiou"
+
+vowelsdata = {}.fromkeys(vowels, 0)
+
+for character in new_string1:
+    if character in vowels:
+        vowelsdata[character] += 1
+for vowel in vowelsdata:
+    print(vowel, " -> " , vowelsdata[vowel])
+
